5ff4414df954651bdb6c7974ad2932104effc547
Arisuuuuu <casipong.131634@cubao.sti.edu.ph>
2024-10-09 02:59:47 +0800

    Update dropdown.tsx

--- src/components/ui/dropdown.tsx
+++ src/components/ui/dropdown.tsx
@@ -4,49 +4,49 @@ import { DropdownMenuCheckboxItemProps } from "@radix-ui/react-dropdown-menu"
 import * as React from "react"
 
 import {
-    DropdownMenu,
-    DropdownMenuCheckboxItem,
-    DropdownMenuContent,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { Button } from "./button"
 
 type Checked = DropdownMenuCheckboxItemProps["checked"]
 
-export default function dropdown(Open:string) {
+export default function Dropdown(Open: string) {
   const [showStatusBar, setShowStatusBar] = React.useState<Checked>(true)
   const [showActivityBar, setShowActivityBar] = React.useState<Checked>(false)
   const [showPanel, setShowPanel] = React.useState<Checked>(false)
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline">{Open}</Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56">
-        <DropdownMenuLabel>Appearance</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuCheckboxItem
-          checked={showStatusBar}
-          onCheckedChange={setShowStatusBar}
-        >
-          On Leave
-        </DropdownMenuCheckboxItem>
-        <DropdownMenuCheckboxItem
-          checked={showActivityBar}
-          onCheckedChange={setShowActivityBar}
-        >
-          In Office
-        </DropdownMenuCheckboxItem>
-        <DropdownMenuCheckboxItem
-          checked={showPanel}
-          onCheckedChange={setShowPanel}
-        >
-          In Remote
-        </DropdownMenuCheckboxItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+  <DropdownMenu>
+    <DropdownMenuTrigger asChild>
+    <Button variant="outline">{Open}</Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent className="w-56">
+    <DropdownMenuLabel>Appearance</DropdownMenuLabel>
+    <DropdownMenuSeparator />
+    <DropdownMenuCheckboxItem
+      checked={showStatusBar}
+      onCheckedChange={setShowStatusBar}
+    >
+      On Leave
+    </DropdownMenuCheckboxItem>
+    <DropdownMenuCheckboxItem
+      checked={showActivityBar}
+      onCheckedChange={setShowActivityBar}
+    >
+      In Office
+    </DropdownMenuCheckboxItem>
+    <DropdownMenuCheckboxItem
+      checked={showPanel}
+      onCheckedChange={setShowPanel}
+    >
+      In Remote
+    </DropdownMenuCheckboxItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
   )
 }
