0d3e22872aef65b98abbb71351f8325eff465746
James Casipong <144509235+jamescasipong@users.noreply.github.com>
2024-10-14 21:46:43 +0800

    xd

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { SideThemeProvider } from "@/contextComponent/SideDark";
 import localFont from "next/font/local";
 import Navbar from "@/customComponents/navbar";
+import { useContext, useEffect } from "react";
+import { SideDark } from "@/contextComponent/SideDark";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -15,8 +17,22 @@ const geistMono = localFont({
 });
 
 const App = ({ children }: any) => {
+  const { isDarkMode } = useContext(SideDark) || { isDarkMode: false }; // Default to false
+
+  useEffect(() => {
+    const savedState = localStorage.getItem("isDarkMode");
+    if (savedState) {
+      document.documentElement.classList.toggle("dark", JSON.parse(savedState));
+    } else {
+      const prefersDarkMode = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+      ).matches;
+      document.documentElement.classList.toggle("dark", prefersDarkMode);
+    }
+  }, []);
+
   return (
-    <html lang="en">
+    <html lang="en" className={isDarkMode ? "dark" : ""}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-100 dark:bg-gray-900 transition-colors duration-200`}
       >
