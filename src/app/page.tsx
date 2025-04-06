554f3e399bc0fdc4d66e4d3b5e2dfc8fa4dbaee2
Arisuuuuu <jamesxcasipong@gmail.com>
2025-04-06 10:22:49 +0800

    add footer in homepage :))

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import HomePage from "@/app/components/HomePage";
-import Footer from "./components/hero";
+import {Footer} from "./(home)/components/footer";
 
 export const metadata = {
     title: 'HRConnect - Simplify Your HR Management',
