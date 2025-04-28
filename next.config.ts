bf41096e7a3c99e14a36013b040e5bc69a29010f
Arisuuuuu <jamesxcasipong@gmail.com>
2025-04-28 21:48:29 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -16,7 +16,6 @@ const nextConfig: NextConfig = {
     },
     env: {
         "NEXT_PUBLIC_API_URL": process.env.NEXT_PUBLIC_API_URL,
-        "NODE_ENV": process.env.NODE_ENV,
     },
 
     async redirects() {
